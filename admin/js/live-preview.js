a162fc4a12164ce07b2dce8eee1d157cc5baa784
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2019-04-24 15:18:05 -0400

    cleaned up live-preview code, eleminating as much liquid as possible and using better es6

--- admin/js/live-preview.js
+++ admin/js/live-preview.js
@@ -1,13 +1,22 @@
 ---
 ---
 
+fetch("/admin/config.yml").then(r => r.text()).then(data => generatePreviews(data));
 CMS.registerPreviewStyle("/css/main.css");
 
+var docs = [
+    {% for doc in site.documents %}
+        {
+            collection: '{{ doc.collection }}',
+            path: '{{ doc.path }}',
+            html: '{{ doc | strip_newlines | split: "<body" | last | split: "</body>" | first | prepend: "<div" | append: "</div>" }}'
+        },
+    {% endfor %}
+];
 var fieldsRegEx = /<!-- *?field +?(\w+?)="(\w*?)".*?-->.*?<!-- *?\/field *?-->/;
 
-function updateHTML (html, props, regEx) {
-    var regEx = arguments.length > 2 && arguments [2] != undefined ? arguments[2] : fieldsRegEx;
-    var match, replace;
+const updateHTML = (html, props, regEx = fieldsRegEx) => {
+    let match, replace;
     while (match = regEx.exec(html)) {
         if (match[1] == "name") {
             replace = props.entry.getIn(["data", match[2]]);
@@ -19,35 +28,18 @@ function updateHTML (html, props, regEx) {
     return html;
 }
 
-{% for c in site.collections %}
-
-    // make a map of file slugs and their html content
-    var {{ c.label }} = {
-        {% for doc in c.docs %}
-            {% assign template = doc | strip_newlines | split: '<body' | last | split: '</body>' | first | prepend: '<div' | append: '</div>' %}
-            "{{ doc.path }}" : '{{ template }}'{% unless forloop.last %},{% endunless %}
-        {% endfor %}
-    }
-
-    // generate a live preview
-    CMS.registerPreviewTemplate("{{ c.label }}", createClass({
-        render () {
-            var html = {{ c.label }}[this.props.entry.get("path")]; // use html content from page with corresponding slug
-            return h("div", { "dangerouslySetInnerHTML" : {__html: updateHTML(html, this.props) } });
-        }
-    }));
-
-{% endfor %}
-
-{% for c in site.campaigns %}
-    {% unless c.sub_campaign %}
-
-        CMS.registerPreviewTemplate("{{ c.slug }}", createClass({
+const generatePreviews = yaml => {
+    const config = jsyaml.load(yaml);
+    config.collections.forEach(c => {
+        const collection = c.folder.match(/_(.*?)(?:\/.*)?$/)[1]; // don't creat this variable, name is too confusing
+        const cDocs = docs.filter(d => d.collection === collection); // may not even need to do this...can i just use the document path? should always be unique? --> right now collection just serves as a way to get a generic layout for new docs
+        CMS.registerPreviewTemplate(c.name, createClass({
             render () {
-                var html = campaigns[this.props.entry.get("path")]; // use html content from page with corresponding slug
+                const path = this.props.entry.get("path");
+                const match = cDocs.find(d => d.path === path);
+                const html = match ? match.html : cDocs[0].html;
                 return h("div", { "dangerouslySetInnerHTML" : {__html: updateHTML(html, this.props) } });
             }
         }));
-
-    {% endunless %}
-{% endfor %}
+    })
+}
